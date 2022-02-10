30bef09bd3fe2fb09b3d23d6cd7aa2edc7abb029
Yuliya Chekurova <yuliya.chekurova@smartseeds.ru>
2022-02-10 07:56:32 +0300

    Fix comment

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -31,8 +31,8 @@ public class PracticeFormTest {
         $(byText("Male")).click();
         // Номер телефона
         $("#userNumber").setValue("1234567890");
-        Selenide.sleep(2000);
-        $(("#submit")).click();
+        Selenide.sleep(3000);
+        $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave((textCaseSensitive("Thanks for submitting the form")));
         $(".table-responsive").shouldHave(
@@ -48,37 +48,36 @@ public class PracticeFormTest {
     void fillAllFieldsTest() {
         open("/automation-practice-form");
         // Name LastName Emai;
-        Selenide.$(".main-header").shouldHave(text("Practice Form"));
-        Selenide.$("#firstName").setValue("Bob");
-        Selenide.$("#lastName").setValue("Alex");
-        Selenide.$("#userEmail").setValue("bobalex@gmail.com");
+        $(".main-header").shouldHave(text("Practice Form"));
+        $("#firstName").setValue("Bob");
+        $("#lastName").setValue("Alex");
+        $("#userEmail").setValue("bobalex@gmail.com");
         // Gender
-        Selenide.$(byText("Male")).click();
+        $(byText("Male")).click();
         // Phone namber
-        Selenide.$("#userNumber").setValue("1234567890");
+        $("#userNumber").setValue("1234567890");
         // Calendar
-//          setDateById("dateOfBirthInput", "16.02.2000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("2");
         $(".react-datepicker__year-select").selectOptionByValue("2000");
         $(".react-datepicker__day--016").click();
 
         // Hobbies
-        Selenide.$(byText("Reading")).click();
-        Selenide.$(byText("Music")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
         // Subjects
         $("#subjectsInput").setValue("Arts").pressEnter();
         // Load file
         File fl = new File("src/test/resources/Pic.png");
-        Selenide.$("#uploadPicture").uploadFile(fl);
+        $("#uploadPicture").uploadFile(fl);
         //Current Address
-        Selenide.$("#currentAddress").setValue("cat dog chicken 15");
+        $("#currentAddress").setValue("cat dog chicken 15");
         // Select State
-        Selenide.$("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
         // Select City
-        Selenide.$("#react-select-4-input").setValue("Agra").pressEnter();
+        $("#react-select-4-input").setValue("Agra").pressEnter();
         // Button #submit
-        $(("#submit")).click();
+        $("#submit").click();
         // Asserts
         $("#example-modal-sizes-title-lg").shouldHave((textCaseSensitive("Thanks for submitting the form")));
         $(".table-responsive").shouldHave(
@@ -94,12 +93,5 @@ public class PracticeFormTest {
                 textCaseSensitive("State and City"), textCaseSensitive("Uttar Pradesh Agra")
         );
 
-    }
-
-    void setDateById(String name, String date) {
-        executeJavaScript(
-                String.format("$('[id=\"%s\"]').val('%s')",
-                        name, date)
-        );
     }
 }
